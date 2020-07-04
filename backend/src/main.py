2ccbbecd6581422eddecbdf9dc3736ad3344f022
Jesus Sanchez <jesus.san.sev@gmail.com>
2020-07-03 18:59:24 -0700

    added fask endpoints

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,25 +1,34 @@
+from flask import Flask, jsonify, request
 from .entities.entity import Session, engine, Base
-from .entities.exam import Exam
+from .entities.exam import Exam, ExamSchema
+
+# create the Flask app
+app = Flask(__name__)
 
 # generate database schema
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
+@app.route('/exams')
+def get_exams():
+    # start session
+    session = Session()
+    exam_objects = session.query(Exam).all()
+    schema = ExamSchema(many=True)
+    exams = schema.dump(exam_objects)
 
-# check for existing data
-exams = session.query(Exam).all()
-
-if len(exams) == 0:
-    # create and persist dummy exam
-    python_exam = Exam("SQLAlchemy Exam", "Test your knowledge about SQLAlchemy.", "script")
-    session.add(python_exam)
-    session.commit()
     session.close()
-    # reload exams
-    exams = session.query(Exam).all()
+    return jsonify(exams)
 
-# show existing exams
-print('### Exams:')
-for exam in exams:
-    print(f'({exam.id}) {exam.title} - {exam.description}')
+@app.route('/exams', methods=['POST'])
+def add_exam():
+    posted_exam = ExamSchema(only=('title', 'description')).load(request.get_json())
+    print(posted_exam)
+    exam = Exam(**posted_exam, created_by="HTTP post request")
+
+    session = Session()
+    session.add(exam)
+    session.commit()
+
+    new_exam = ExamSchema().dump(exam)
+    session.close()
+    return jsonify(new_exam), 201
